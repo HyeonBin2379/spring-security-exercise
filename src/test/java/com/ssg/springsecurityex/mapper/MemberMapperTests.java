692c7809828420ad56a6deebbfd2e5b253ebd5a4
Lee Hyeon Bin <hyeonbin2379@gmail.com>
2025-11-12 21:34:39 +0900

    import문 정리

--- src/test/java/com/ssg/springsecurityex/mapper/MemberMapperTests.java
+++ src/test/java/com/ssg/springsecurityex/mapper/MemberMapperTests.java
@@ -16,7 +16,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
